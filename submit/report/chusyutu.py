cea79540856db569e3d52a6ec074100dd5c1c30b
ShibasakiTakuya <83163537+ShibasakiTakuya@users.noreply.github.com>
2021-12-09 16:08:16 +0900

    Add files via upload

---
+++ submit/report/chusyutu.py
@@ -0,0 +1,18 @@
+# ファイルをオープンして、1行ずつその内容を読み込んで処理する
+with open('test.csv','r',newline='') as f:
+    for line in f:
+        line = line.rstrip()  # 読み込んだ行の末尾には改行文字があるので削除
+        print(line)
+# 出力結果（4行目に空行が表示されるときとされないときがあるのを除き、以下同じ）
+
+# テキストファイルをオープンして、その内容を全て読み込み、クローズする
+f = open('test.csv','r',newline='')  # f = open('test.txt', 'rt'):
+s = f.read()  # ファイルの全内容が1つの文字列として返される
+print("総文字数は" + str(len(s)) +"文字")
+f.close()
+
+# with文と組み合わせると使い終わったとき（ブロック終了時）や
+# 例外が発生したときにファイルが自動的にクローズされる
+with open('test.csv','r',newline='') as f:
+    s = f.read()
+
